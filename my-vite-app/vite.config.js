7afc8cea96106ab80ea50d47b07c2afad0a42579
renx19 <1markjustinborja1@gmail.com>
2024-12-10 00:39:35 +0800

    Update vite.config.js

--- my-vite-app/vite.config.js
+++ my-vite-app/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/my-vite-app', // Ensure this is set to the root
+    base: '/Quiz-App/my-vite-app', // Ensure this is set to the root
 })
