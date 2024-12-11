509957e639ab3f28213492cb10316a14998fba76
renx19 <1markjustinborja1@gmail.com>
2024-12-11 21:37:42 +0800

    m

--- my-vite-app/vite.config.js
+++ my-vite-app/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
-
+import icon1 from './src/assets/android-launchericon-192-192.png'
+import icon2 from './src/assets/android-launchericon-512-512.png'
+import icon3 from './src/assets/512.png'
 export default defineConfig({
     plugins: [
         react(),
@@ -15,17 +17,21 @@ export default defineConfig({
                 background_color: '#ffffff',
                 display: 'standalone',
                 icons: [{
-                        src: '/android-launchericon-192-192.png',
+                        // src: '/android-launchericon-192-192.png',
+                        src: icon1,
                         sizes: '192x192',
                         type: 'image/png',
                     },
                     {
-                        src: '/android-launchericon-512-512.png',
+                        // src: '/android-launchericon-512-512.png',
+                        src: icon2,
                         sizes: '512x512',
                         type: 'image/png',
                     },
                     {
-                        src: '/512.png',
+
+                        // src: '/512.png',
+                        src: icon3,
                         sizes: '512x512',
                         type: 'image/png',
                         purpose: 'any maskable',
