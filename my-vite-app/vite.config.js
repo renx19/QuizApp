8cd94e1391932d9b0e2ae9677d725e02052881eb
renx19 <1markjustinborja1@gmail.com>
2024-12-11 21:17:42 +0800

    pwa

--- my-vite-app/vite.config.js
+++ my-vite-app/vite.config.js
@@ -1,8 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Quiz App',
+                short_name: 'Quiz App',
+                description: 'Quiz App',
+                theme_color: '#ffffff',
+                background_color: '#ffffff',
+                display: 'standalone',
+                icons: [{
+                        src: './public/android-launchericon-192-192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: './public/android-launchericon-512-512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: './public/512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                ],
+            },
+        }),
+    ],
     build: {
         rollupOptions: {
             // You can add custom rollup options here
