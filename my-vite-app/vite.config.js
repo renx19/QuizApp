cbad3af0880980e28ab1c32f50d0075c153e7522
renx19 <1markjustinborja1@gmail.com>
2024-12-11 21:26:32 +0800

    Update vite.config.js

--- my-vite-app/vite.config.js
+++ my-vite-app/vite.config.js
@@ -15,17 +15,17 @@ export default defineConfig({
                 background_color: '#ffffff',
                 display: 'standalone',
                 icons: [{
-                        src: './public/android-launchericon-192-192.png',
+                        src: '/android-launchericon-192-192.png',
                         sizes: '192x192',
                         type: 'image/png',
                     },
                     {
-                        src: './public/android-launchericon-512-512.png',
+                        src: '/android-launchericon-512-512.png',
                         sizes: '512x512',
                         type: 'image/png',
                     },
                     {
-                        src: './public/512.png',
+                        src: '/512.png',
                         sizes: '512x512',
                         type: 'image/png',
                         purpose: 'any maskable',
