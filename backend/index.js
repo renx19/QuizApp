bc8e3cb1676e3607a1b6ab6dbef0fba9b034dfc7
renx19 <138985568+renx19@users.noreply.github.com>
2026-01-14 22:49:08 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use("/", questionsRouter);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${PORT}`);
 });
 // Export for serverless
 // module.exports = app;
